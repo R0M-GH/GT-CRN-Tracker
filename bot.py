ae030f4331fb2ef9535e3aa4979b7ef1e940e99f
R0M-GH <rom.malla@outlook.com>
2024-11-19 05:51:31 -0500

    Bender v0.9 (need some feedback first)
    migration and other stuff, we got until may 2025 its calm

--- bot.py
+++ bot.py
@@ -27,19 +27,19 @@ try:
     connection.commit()
     connection.close()
 except sqlite3.Error as e:
-    print(f"Error while creating DB: {e}")
+    print(f'Error while creating DB: {e}')
 
 def get_user_data(chat_id):
     try:
         connection = sqlite3.connect(DB_NAME)
         c = connection.cursor()
-        c.execute("SELECT crns FROM user_data WHERE chat_id = ?", (chat_id,))
+        c.execute('SELECT crns FROM user_data WHERE chat_id = ?', (chat_id,))
         result = c.fetchone()
         connection.close()
         if result: return result[0].split(',')
         return []
     except sqlite3.Error as e:
-        print(f"Error while fetching user data: {e}")
+        print(f'Error while fetching user data: {e}')
         return []
 
 def update_user_data(chat_id, crns):
@@ -47,32 +47,32 @@ def update_user_data(chat_id, crns):
         connection = sqlite3.connect(DB_NAME)
         c = connection.cursor()
         crns_str = ','.join(crns)
-        c.execute("REPLACE INTO user_data (chat_id, crns) VALUES (?, ?)", (chat_id, crns_str))
+        c.execute('REPLACE INTO user_data (chat_id, crns) VALUES (?, ?)', (chat_id, crns_str))
         connection.commit()
         connection.close()
-    except sqlite3.Error as e: print(f"Error while updating user data: {e}")
+    except sqlite3.Error as e: print(f'Error while updating user data: {e}')
 
 def get_user_prefs(chat_id):
     try:
         connection = sqlite3.connect(DB_NAME)
         c = connection.cursor()
-        c.execute("SELECT mute_all, mute_waitlist FROM user_prefs WHERE chat_id = ?", (chat_id,))
+        c.execute('SELECT mute_all, mute_waitlist FROM user_prefs WHERE chat_id = ?', (chat_id,))
         result = c.fetchone()
         connection.close()
         if result: return {'mute_all': result[0], 'mute_waitlist': result[1]}
         return {'mute_all': False, 'mute_waitlist': False}
     except sqlite3.Error as e:
-        print(f"Error while fetching user preferences: {e}")
+        print(f'Error while fetching user preferences: {e}')
         return {'mute_all': False, 'mute_waitlist': False}
 
 def update_user_prefs(chat_id, mute_all, mute_waitlist):
     try:
         connection = sqlite3.connect(DB_NAME)
         c = connection.cursor()
-        c.execute("REPLACE INTO user_prefs (chat_id, mute_all, mute_waitlist) VALUES (?, ?, ?)", (chat_id, mute_all, mute_waitlist))
+        c.execute('REPLACE INTO user_prefs (chat_id, mute_all, mute_waitlist) VALUES (?, ?, ?)', (chat_id, mute_all, mute_waitlist))
         connection.commit()
         connection.close()
-    except sqlite3.Error as e: print(f"Error while updating user preferences: {e}")
+    except sqlite3.Error as e: print(f'Error while updating user preferences: {e}')
 
 commands = [
     {'command': 'list', 'description': 'List all class CRNs being tracked'},
@@ -95,14 +95,14 @@ async def fetch_course_data(session, term, crn):
             except AttributeError:
                 return crn, None, None
     except Exception as e:
-        print(f"Error fetching course data for CRN {crn}: {e}")
+        print(f'Error fetching course data for CRN {crn}: {e}')
         return crn, None, None
 
 async def course_check():
     while True:
         connection = sqlite3.connect(DB_NAME)
         c = connection.cursor()
-        c.execute("SELECT chat_id FROM user_data")
+        c.execute('SELECT chat_id FROM user_data')
         user_ids = [row[0] for row in c.fetchall()]
         connection.close()
 
@@ -130,21 +130,21 @@ def send_course_notification(chat_id, course_str, crn, notif: Literal['waitlist'
         requests.post(url, json={'chat_id': chat_id, 'text': msg, 'reply_markup': keyboard})
         requests.post(url, json={'chat_id': chat_id, 'text': crn})
     except requests.exceptions.RequestException as e:
-        print(f"Error sending course notification for CRN {crn}: {e}")
+        print(f'Error sending course notification for CRN {crn}: {e}')
 
 def send_user_message(chat_id, msg):
     url = f'https://api.telegram.org/bot{BOT_TOKEN}/sendMessage'
     try:
         requests.post(url, json={'chat_id': chat_id, 'text': msg})
     except requests.exceptions.RequestException as e:
-        print(f"Error sending message to {chat_id}: {e}")
+        print(f'Error sending message to {chat_id}: {e}')
 
 def send_user_keyboard(chat_id, msg, keyboard):
     url = f'https://api.telegram.org/bot{BOT_TOKEN}/sendMessage'
     try:
         requests.post(url, json={'chat_id': chat_id, 'text': msg, 'reply_markup': keyboard})
     except requests.exceptions.RequestException as e:
-        print(f"Error sending message to {chat_id}: {e}")
+        print(f'Error sending message to {chat_id}: {e}')
 
 async def telegram_handler():
     offset = None
@@ -171,7 +171,7 @@ async def telegram_handler():
 
                             if text.startswith('/list'):
                                 if not crns:
-                                    send_user_message(chat_id, "No CRNs are being tracked.")
+                                    send_user_message(chat_id, 'No CRNs are being tracked.')
                                 else:
                                     async with ClientSession() as session:
                                         results = await asyncio.gather(*[fetch_course_data(session, term, crn) for crn in crns])
@@ -179,15 +179,15 @@ async def telegram_handler():
                                         course_list = []
                                         for crn, name, data in results:
                                             if name and data:
-                                                course_list.append({'text': f"{name[2]} ({name[3]}) - {crn}", 'callback_data': f'course_{crn}'})
+                                                course_list.append({'text': f'{name[2]} ({name[3]}) - {crn}', 'callback_data': f'course_{crn}'})
                                             else:
-                                                course_list.append({'text': f"CRN {crn} (Invalid or Unavailable)", 'callback_data': f'course_{crn}'})
+                                                course_list.append({'text': f'CRN {crn} (Invalid or Unavailable)', 'callback_data': f'course_{crn}'})
 
                                         if course_list:
                                             keyboard = {'inline_keyboard': [[item] for item in course_list]}
-                                            send_user_keyboard(chat_id, "Click on a course to view details:", keyboard)
+                                            send_user_keyboard(chat_id, 'Click on a course to view details:', keyboard)
                                         else:
-                                            send_user_message(chat_id, "No valid course details found for the tracked CRNs.")
+                                            send_user_message(chat_id, 'No valid course details found for the tracked CRNs.')
 
 
                             elif text.startswith('/add'):
@@ -196,11 +196,11 @@ async def telegram_handler():
                                     if crn not in crns:
                                         crns.append(crn)
                                         update_user_data(chat_id, crns)
-                                        send_user_message(chat_id, f"Added CRN {crn} to tracking.")
+                                        send_user_message(chat_id, f'Added CRN {crn} to tracking.')
                                     else:
-                                        send_user_message(chat_id, f"CRN {crn} is already being tracked.")
+                                        send_user_message(chat_id, f'CRN {crn} is already being tracked.')
                                 except ValueError:
-                                    send_user_message(chat_id, "Invalid format. Use /add {CRN}.")
+                                    send_user_message(chat_id, 'Invalid format. Use /add {CRN}.')
                                 
                             elif text.startswith('/rem'):
                                 try:
@@ -208,27 +208,27 @@ async def telegram_handler():
                                     if crn in crns:
                                         crns.remove(crn)
                                         update_user_data(chat_id, crns)
-                                        send_user_message(chat_id, f"Removed CRN {crn} from tracking.")
+                                        send_user_message(chat_id, f'Removed CRN {crn} from tracking.')
                                     else:
-                                        send_user_message(chat_id, f"CRN {crn} is not being tracked.")
+                                        send_user_message(chat_id, f'CRN {crn} is not being tracked.')
                                 except ValueError:
-                                    send_user_message(chat_id, "Invalid format. Use /rem {CRN}.")
+                                    send_user_message(chat_id, 'Invalid format. Use /rem {CRN}.')
                         
                             elif text.startswith('/mute_all'):
                                 update_user_prefs(chat_id, True, prefs['mute_waitlist'])
-                                send_user_message(chat_id, "All notifications are now muted.")
+                                send_user_message(chat_id, 'All notifications are now muted.')
 
                             elif text.startswith('/unmute_all'):
                                 update_user_prefs(chat_id, False, prefs['mute_waitlist'])
-                                send_user_message(chat_id, "All notifications are now unmuted.")
+                                send_user_message(chat_id, 'All notifications are now unmuted.')
                         
                             elif text.startswith('/mute_waitlist'):
                                 update_user_prefs(chat_id, prefs['mute_all'], True)
-                                send_user_message(chat_id, "Waitlist notifications are now muted.")
+                                send_user_message(chat_id, 'Waitlist notifications are now muted.')
 
                             elif text.startswith('/unmute_waitlist'):
                                 update_user_prefs(chat_id, prefs['mute_all'], False)
-                                send_user_message(chat_id, "Waitlist notifications are now unmuted.")
+                                send_user_message(chat_id, 'Waitlist notifications are now unmuted.')
 
                         elif 'callback_query' in update:
                             callback_query = update['callback_query']
@@ -252,33 +252,33 @@ async def telegram_handler():
                                 if crn in crns:
                                     crns.remove(crn)
                                     update_user_data(chat_id, crns)
-                                    send_user_message(chat_id, f"CRN {crn} removed from tracking.")
+                                    send_user_message(chat_id, f'CRN {crn} removed from tracking.')
                                 else:
-                                    send_user_message(chat_id, f"CRN {crn} is not being tracked.")
+                                    send_user_message(chat_id, f'CRN {crn} is not being tracked.')
 
                             elif callback_data == 'back_to_list':
                                 if crns:
                                     keyboard = {'inline_keyboard': [[{'text': f'CRN: {crn}', 'callback_data': f'course_{crn}'}] for crn in crns]}
-                                    send_user_keyboard(chat_id, "Click on a CRN to view details:", keyboard)
+                                    send_user_keyboard(chat_id, 'Click on a CRN to view details:', keyboard)
             
             current_time = datetime.now()
             connection = sqlite3.connect(DB_NAME)
             cursor = connection.cursor()
-            cursor.execute("SELECT COUNT(*) FROM user_data")
+            cursor.execute('SELECT COUNT(*) FROM user_data')
             user_count = cursor.fetchone()[0]
             connection.close()
 
             refresh_time = asyncio.get_event_loop().time()
-            print(f"\rUsers: \033[92m{user_count}\033[0m | Uptime: \033[91m{str(current_time - start_time).split('.')[0]}\033[0m | " \
-                  f"Refresh: \033[91m{str((current_time - last_refresh).total_seconds()).split('.')[0]}s\033[0m", end="")
+            print(f'\rUsers: \033[92m{user_count}\033[0m | Uptime: \033[91m{str(current_time - start_time).split(".")[0]}\033[0m | ' \
+                  f'Refresh: \033[91m{str((current_time - last_refresh).total_seconds()).split(".")[0]}s\033[0m', end='')
             last_refresh = datetime.now()
 
             await asyncio.sleep(3)
         except Exception as e:
-            print(f"Error in telegram_handler loop: {e}")
+            print(f'Error in telegram_handler loop: {e}')
             await asyncio.sleep(3)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     loop = asyncio.get_event_loop()
     loop.create_task(course_check())
     loop.run_until_complete(telegram_handler())
